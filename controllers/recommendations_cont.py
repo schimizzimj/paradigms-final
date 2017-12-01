b1e8f75fcb1fb2a46a2a9045654c00cdf936efee
Marcus Schimizzi <mschimiz@nd.edu>
2017-12-01 01:45:10 -0500

    fixes

--- controllers/recommendations_cont.py
+++ controllers/recommendations_cont.py
@@ -28,5 +28,6 @@ class RecommendationsController(object):
 		print(ingredient_list)
 		print(key)
 		refined = rdb.get_recipe_by_ingredient(ingredient_list)
+		print(refined)
 		highest = rdb.get_highest_nonrated_recipe(key, refined)
 		return json.dumps({'recipe_id': highest, 'result': 'success'})
